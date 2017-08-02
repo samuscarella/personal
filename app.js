46450bc6551cb617ac42f145e9c9b4ca55d7957d
samuscarella <smuscarella29@gmail.com>
2017-08-01 17:58:29 -0700

    Facebook Share API

--- app.js
+++ app.js
@@ -29,7 +29,35 @@ app.use(function(req, res, next) {
 const port = 9000;
 
 app.listen(port, function () {
-  console.log("Example app listening on port " + port + "!'")
-})
+  console.log("Example app listening on port " + port + "!'");
+  logHostIpAddress();
+});
+
+function logHostIpAddress() {
+    'use strict';
+
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
+
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                console.log(ifname + ':' + alias, iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                console.log(ifname, iface.address);
+            }
+            ++alias;
+        });
+    });
+}
 
 module.exports = app;
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,3 @@
-
 var idx = 0;
 
 var portfolio = ['<div id="popcorn" class="container-fluid portfolio-container"> <div class="row"> <div class="col-md-12 navigator"> <a id="decrement"><img src="/images/arrow-left.png" class="arrows left-arrow"></a><img src="/images/popcorn-logo.png" class="ma-and-pa-main-image"><a id="increment"><img src="/images/arrow-right.png" class="arrows right-arrow"/></a></div></div> <div class="row"> <div class="col-md-12"><!-- <h1 class="main-project-title">Popcorn</h1>--> </div> </div> <div class="row app-img-row"> <div class="col-md-4 app-img-col"> <img src="/images/mapa-login.png" class="app-img float-right" id="app-img-seven"> </div> <div class="col-md-4 app-img-col"> <img src="/images/mapa-shop.png" class="app-img" id="app-img-eight"> </div> <div class="col-md-4 app-img-col"> <img src="/images/mapa-products.png" class="app-img float-left" id="app-img-nine"> </div> </div> <div class="row app-img-row"> <div class="col-md-4 app-img-col"> <img src="/images/mapa-cart.png" class="app-img float-right" id="app-img-ten"> </div> <div class="col-md-4 app-img-col"> <img src="/images/mapa-blue.png" class="app-img" id="app-img-eleven"> </div> <div class="col-md-4 app-img-col"> <img src="/images/mapa-form.png" class="app-img float-left" id="app-img-twelve"> </div> </div></div>',
@@ -8,33 +7,65 @@ var portfolio = ['<div id="popcorn" class="container-fluid portfolio-container">
 
 var content = ['<div id="blast" class="container-fluid blast-container"><div class="row"><div class="col-md-12"><h1 class="blast-title"><img src="/images/layers.png" class="general-icon"/>Mobile Solutions</h1><h3 class="blast-description">Sharp-minded iOS developer who writes highly readable, clean, maintainable source code. Experience in iOS mobile application development using Swift, iPhone SDK, and Cocoa Touch framework. Expertise in development of iPhone applications, consuming all the required API\'s for mobile applications in an efficient way. Results include robust downloads, activations, session lengths, retention rates, revenue increases and referrals. Demonstrated success developing iOS apps written in Swift using a variety of technologies. Proven ability to manage multiple tasks and projects simultaneously, prioritizing each for maximum effectiveness and efficiency. Extensive experience testing enterprise level applications manually and with automation to report, fix, and correct any bugs or defects in the software. I\'m constantly pushing myself to learn the latest technologies including Artificial Intelligence and all forms of Information Security. Driven by new challenges and the desire to excel in all endeavors.</h3></div></div></div>'];
 
+const facebookAppId = 138291876767981;
+
 $(document).ready(function() {
+
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: facebookAppId,
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: 'v2.10'
+        });
+        FB.AppEvents.logPageView();
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
     execute();
 
-    $(document).on("click", "#decrement", function() {
+    $(document).on("click", "#decrement", function () {
         decrement();
         $("#popcorn img").fadeOut("fast");
         $("#popcorn img").fadeIn("fast");
     });
 
-    $(document).on("click", "#increment", function() {
+    $(document).on("click", "#increment", function () {
         increment();
         $("#popcorn img").fadeOut("fast");
         $("#popcorn img").fadeIn("fast");
     });
 
-    $("#general").on("click", function() {
+    $("#general").on("click", function () {
         $('#space').html(content[0]);
     });
 
-    $("#portfolio").on("click", function() {
+    $("#portfolio").on("click", function () {
         $('#space').html(portfolio[0]);
     });
 
-    $("#contact-me").on('click', function() {
-        window.scrollTo(0,document.body.scrollHeight);
+    $("#contact-me").on('click', function () {
+        window.scrollTo(0, document.body.scrollHeight);
     });
+
+    document.getElementById('social-icon-one').onclick = function () {
+        FB.ui({
+            method: 'share',
+            display: 'popup',
+            href: 'https://developers.facebook.com/docs/',
+        }, function (response) {
+        });
+    };
 });
 
 function increment() {
@@ -59,4 +90,4 @@ function decrement() {
 
 function execute() {
     $('#space').html(portfolio[idx]);
-}
+};
